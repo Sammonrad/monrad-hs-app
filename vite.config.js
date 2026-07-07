dfcd64bba4b2cb27d769194322a5d8a0e4115b0a
Sam Monrad <sammonrad@icloud.com>
2026-07-07 18:48:20 +1200

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: null,
+      manifest: false,
+      includeAssets: [
+        'favicon.svg',
+        'manifest.webmanifest',
+        'monrad-logo-cropped.jpg',
+        'pwa-192.svg',
+        'pwa-512.svg',
+      ],
+      workbox: {
+        globPatterns: [],
+        navigateFallback: null,
+        runtimeCaching: [],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
